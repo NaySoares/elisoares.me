12c52825a62f736620a1b1dbf84483e1196fcbc5
Elienai <elienay.soares07@gmail.com>
2021-12-10 12:47:35 -0300

    fix nav

--- components/navbar.js
+++ components/navbar.js
@@ -15,6 +15,7 @@ import {
   position,
 } from "@chakra-ui/react";
 import { HamburgerIcon } from "@chakra-ui/icons";
+import ThemeToggleButton from './theme-toggle-button'
 
 const LinkItem = ({ href, path, _target, children, ...props }) => {
   const active = path === href;
@@ -78,6 +79,7 @@ const Navbar = (props) => {
         </Stack>
 
         <Box flex={1} align="right">
+          <ThemeToggleButton />
           <Box ml={2} display={{ base: "inline-block", md: "none" }}>
             <Menu>
               <MenuButton
