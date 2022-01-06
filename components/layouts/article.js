b21beca1c72073fc083c45ac307c827ecca5919f
Elienai <elienay.soares07@gmail.com>
2022-01-06 14:09:17 -0300

    fix(favicon): favicon version png

--- components/layouts/article.js
+++ components/layouts/article.js
@@ -20,7 +20,7 @@ const Layout = ({ children, title }) => (
     <>
       {title && (
         <Head>
-          <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+          <link rel="shortcut icon" href="/favicon.png" type="image/x-icon" />
           <title>{title} - E.Soares</title>
         </Head>
       )}
