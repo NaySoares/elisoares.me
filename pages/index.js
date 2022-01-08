2741797023f66361e96c4299796532751e07ef7b
Elienai <elienay.soares07@gmail.com>
2022-01-08 19:23:21 -0300

    add(index): add link gallery

--- pages/index.js
+++ pages/index.js
@@ -72,7 +72,7 @@ const Page = () => {
             de um leve apego para com as anima&ccedil;&otilde;es s&atilde;o os
             pontos que se mostram em destaque. Caso tenha curiosidade confira
             algumas imagens editadas aqui:{" "}
-            <NextLink href="/works">
+            <NextLink href="https://editions-gallery.vercel.app">
               <Link>Galeria</Link>
             </NextLink>
             .
