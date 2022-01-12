75085713f4b2f43eb44d0359e890a95629eb4eda
Elienai <elienay.soares07@gmail.com>
2022-01-11 22:40:34 -0300

    .

--- components/grid-item.js
+++ components/grid-item.js
@@ -22,9 +22,9 @@ export const GridItem = ({ children, thumbnail, href, title }) => (
         width="250"
         loading="lazy"
       />
-      <LinkOverlay href={href}>
+      <Link href={href}>
         <Text mt={2}>{title}</Text>
-      </LinkOverlay>
+      </Link>
       <Text fontSize={14}>{children}</Text>
     </LinkBox>
   </Box>
@@ -42,11 +42,11 @@ export const WorkGridItem = ({ children, path, id, title, thumbnail }) => (
           height="300"
           width="500"
         />
-        <LinkOverlay href={`${path}/${id}`}>
+        <Link href={`${path}/${id}`}>
           <Text mt={2} fontSize={20} fontWeight="bold">
             {title}
           </Text>
-        </LinkOverlay>
+        </Link>
         <Text fontSize={14}>{children}</Text>
       </LinkBox>
     </Link>
@@ -69,13 +69,13 @@ export function SnippetGridItem({ language, href, title, date }) {
       }}
     >
       <LinkBox cursor="pointer">
-        <LinkOverlay href={href}>
+        <Link href={href}>
           <HStack gap="10" my={2} py={3} px={2} justify="space-between">
             <Text>{title}</Text>
             <Text>{language}</Text>
             <Text>{date}</Text>
           </HStack>
-        </LinkOverlay>
+        </Link>
       </LinkBox>
     </Box>
   );
